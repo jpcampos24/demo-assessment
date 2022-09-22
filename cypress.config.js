e6b53ab21a48d17dafa7baa640e2de37121fca5c
jpcampos24 <juanpa_campos@utp.edu.co>
2022-09-21 21:41:48 -0500

    adding dockerhub

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        console.log(launchOptions.args)
+    
+        if (browser.name == 'chrome') {
+          launchOptions.args.push('--disable-gpu')
+        }
+    
+        return launchOptions
+      })
     },
   },
 };
