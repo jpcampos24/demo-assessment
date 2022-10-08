9a75e75b645870aae7467e410e9de392d7a6b162
jpcampos24 <juanpa_campos@utp.edu.co>
2022-10-08 10:49:43 -0500

    first API test cases

--- cypress/e2e/login-page.cy.js
+++ cypress/e2e/login-page.cy.js
@@ -8,20 +8,12 @@ describe('Login into OragenHRM page', () => {
     //setUp page size
     beforeEach(() => {
         homePage.setUpViewport()
-    })
-
-    //load the main page
-    it('sould navigate to orange page', () => {
         homePage.navigate()
         homePage.validateHomePage()
     })
 
     //login into the main page
     it('should send the credentials and login', () => {
-        homePage.loginUser('Admin')
-        homePage.loginPassword('admin123')
-        homePage.loginButton()
-        //this should be fixed
         homePage.loginUser('Admin')
         homePage.loginPassword('admin123')
         homePage.loginButton()
