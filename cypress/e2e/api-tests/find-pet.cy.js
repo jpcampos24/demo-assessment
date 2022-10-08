6ea8b3910c0b9047c479ba31956e0731874ae4ca
jpcampos24 <juanpa_campos@utp.edu.co>
2022-10-08 10:41:45 -0500

    modyfing jenkinsFile

--- cypress/e2e/api-tests/find-pet.cy.js
+++ cypress/e2e/api-tests/find-pet.cy.js
@@ -8,7 +8,7 @@ describe('Finds Pets by status', () => {
                 url: '/pet/findByStatus?status=available'
             }).then(({status, body}) => {
                 expect(status).to.eq(200)
-                expect(body[0].name).to.eq('String')
+                expect(body[0].name).to.be.a('String')
             })
         })
     })
